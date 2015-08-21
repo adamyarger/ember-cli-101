7bb8b5ce839fc92f3a878a1d4cfb536e14076cb5
adamyarger <yargeradam@gmail.com>
2015-08-21 17:23:59 -0500

    adds computed action

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,14 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    {
+      get() {
+      		return !Ember.isEmpty(this.get('model.email')) &&
+          !Ember.isEmpty(this.get('model.firstName')) &&
+          !Ember.isEmpty(this.get('model.lastName')) &&
+          !Ember.isEmpty(this.get('model.twitter'));
+      }
+    }
+  ),
 	actions: {
-		save() {
-			console.log('+- save action in friends new controller');
-			return true;
-		},
-		cancel() {
-			console.log('+- cancel action in friends new controller');
-			return true;
-		}
+	  save() {
+	    if (this.get('isValid')) {
+	      this.get('model').save().then((friend) => {
+	        this.transitionToRoute('friends.show', friend);
+	      });
+	    } else {
+	      this.set('errorMessage', 'You have to fill all the fields');
+	    }
+
+	    return false;
+	  },
+	  cancel() {
+	    this.transitionToRoute('friends');
+
+	    return false;
+	  }
 	}
 });
+
+
