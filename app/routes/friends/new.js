3df61238869f5e16bcbd768a32cad089d6d394c9
adamyarger <yargeradam@gmail.com>
2015-08-20 19:04:14 -0500

    adds preview to form

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.createRecord('friend');
+	},
+	actions: {
+	  save() {
+	    console.log('+-- save action bubbled up to friends new route');
+
+	    return true;
+	  },
+	  cancel() {
+	    console.log('+-- cancel action bubbled up to friends new route');
+
+	    return true;
+	  }
 	}
 });
+
+
