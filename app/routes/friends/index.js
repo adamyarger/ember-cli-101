441f8a0cc2fd60e1840b5865e6bf6a1b7808fc09
adamyarger <yargeradam@gmail.com>
2015-08-21 19:10:42 -0500

    moves delete sction into friends base route

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
-  },
-  actions: {
-  	delete(friend) {
-  		friend.destroyRecord();
-  		return false;
-  	}
   }
 });
--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	actions: {
-		delete(friend) {
-			friend.destroyRecord().then(() => {
-				this.transitionTo('friends.index');
-			});
-		}
-	}
+	
 });
