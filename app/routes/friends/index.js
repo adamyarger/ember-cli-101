2c9b5f7a64c320b7eabe830aba7bf336c436fb6d
adamyarger <yargeradam@gmail.com>
2015-08-21 19:02:42 -0500

    adds delete action

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('friend');
+  },
+  actions: {
+  	delete(friend) {
+  		friend.destroyRecord();
+  		return false;
+  	}
   }
 });
